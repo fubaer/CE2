f72d7e9602a6c2ae3df6480d6b09520f004b899a
Zhengyang <fubaer@hotmail.com>
2014-09-13 17:27:58 +0800

    done: junit for clear and display

--- src/textbuddy/TextBuddyPlusTest.java
+++ src/textbuddy/TextBuddyPlusTest.java
@@ -10,13 +10,18 @@ public class TextBuddyPlusTest {
 	public void test() {
 
 		// Test adding hello
-		assertEquals(TextBuddyPlus.executeCommand("add hellow"),"added to text: \"hellow\"");
+		assertEquals(TextBuddyPlus.executeCommand("add hellow"),"added to text.txt: \"hellow\"");
 
 		// Test adding blank
 		assertEquals(TextBuddyPlus.executeCommand("add"),"Invalid Command issued!");
 		
 		// Test adding number
-		assertEquals(TextBuddyPlus.executeCommand("add 123"),"added to text: \"123\"");
+		assertEquals(TextBuddyPlus.executeCommand("add 123"),"added to text.txt: \"123\"");
+		
+		// Test display after clear
+		TextBuddyPlus.executeCommand("clear");
+		assertEquals(TextBuddyPlus.executeCommand("display"),"text.txt is empty");
+		
 		
 	}
 
